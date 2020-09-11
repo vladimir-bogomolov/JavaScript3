563c15d3f949cff8edb502da1eb0a846492b7b5d
Vladimir <ecofovka@gmail.com>
2020-09-11 17:05:31 +0200

    exercise 3 is done

--- Week2/homework/pokemon-app/script.js
+++ Week2/homework/pokemon-app/script.js
@@ -6,24 +6,42 @@ function fetchData(url, callback) {
         .then(data => {
             return callback(data);
         })
-        .catch(err => console.log(err));
+        .catch(err => {
+            document.getElementById('showImg').innerHTML = `Something went wrong!`;
+            throw new Error('Something went wrong!');
+        });
 }
 
-function addPokemonToDOM() {
-
+function addPokemonToDOM(res) {
+    document.getElementById('showImg').innerHTML = '';
+    let img = document.createElement('img');
+    img.src = res.sprites.front_shiny;
+    document.getElementById('showImg').appendChild(img);
 }
 
 function main() {
+    let getButton = document.createElement('button');
+    getButton.innerText = 'Get Pokemon!';
+    document.body.appendChild(getButton);
     let input = document.createElement('select');
     document.body.appendChild(input);
-    fetchData('https://pokeapi.co/api/v2/pokemon/', (res) => {
-        res.results.forEach(obj => {
-            let option = document.createElement('option');
-            option.innerText = obj.name;
-            input.appendChild(option);
-        });
+    let showImgDiv = document.createElement('div');
+    showImgDiv.setAttribute('id', 'showImg');
+    document.body.appendChild(showImgDiv);
+    getButton.addEventListener('click', (e) => {
+            input.innerHTML = '';
+            fetchData('https://pokeapi.co/api/v2/pokemon/', (res) => {
+                res.results.forEach(obj => {
+                    let option = document.createElement('option');
+                    option.innerText = obj.name;
+                    input.appendChild(option);
+                });
+            })
     });
-    
+    input.addEventListener('change', e => {
+        showImgDiv.innerHTML = '';
+        fetchData(`https://pokeapi.co/api/v2/pokemon/${e.target.value}`, addPokemonToDOM);
+    });
 }
 
 window.onload = main;
