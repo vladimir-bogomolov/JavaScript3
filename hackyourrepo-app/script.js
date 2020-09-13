ebe8805423b06be696c672649f710a17aece14aa
Vladimir <ecofovka@gmail.com>
2020-09-13 22:24:50 +0200

    deleted placeholder data in HY repo app

--- hackyourrepo-app/script.js
+++ hackyourrepo-app/script.js
@@ -1,43 +1,5 @@
 "use strict";
 
-const placeholderRepos = [
-  {
-    name: 'SampleRepo1',
-    description: 'This repository is meant to be a sample',
-    forks: 5,
-    updated: '2020-05-27 12:00:00',
-  },
-  {
-    name: 'AndAnotherOne',
-    description: 'Another sample repo! Can you believe it?',
-    forks: 9,
-    updated: '2020-05-27 12:00:00',
-  },
-  {
-    name: 'HYF-Is-The-Best',
-    description:
-      "This repository contains all things HackYourFuture. That's because HYF is amazing!!!!",
-    forks: 130,
-    updated: '2020-05-27 12:00:00',
-  },
-];
-
-const contribPlaceholder = [{
-  name: 'vladimir-bogomolov',
-  avatar: 'https://avatars2.githubusercontent.com/u/67100332?s=400&u=addba4462934ca7e1a628ecb7e67cd23ebc0418b&v=4',
-  contributions: 10
-}, {
-  name: 'NoerGitKat',
-  avatar: 'https://avatars1.githubusercontent.com/u/13186712?s=460&u=37b2314606d88617b57f5068bbc5d942805c30d8&v=4',
-  contributions: 100
-}];
-
-
-
-
-
-
-
 window.onload = () => {
   // Creating HTML structure
   let containerDiv = document.createElement('div');
@@ -140,3 +102,6 @@ window.onload = () => {
       console.log(error);
     });
 }
+
+// Add if status===ok
+// Add error handlers
